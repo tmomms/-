1cf421a518e3e5da0a4f374029c33f837fef57f6
Bekzat Bakirov <bekzat.bakirov.alt@gmail.com>
2022-07-15 01:32:14 +0600

    finish project

---
+++ js.js
@@ -0,0 +1,75 @@
+const arrowBottom = document.querySelector(".arrow-bottom");
+const arrowBottom2 = document.querySelector(".arrow-bottom2");
+const arrowBottom3 = document.querySelector(".arrow-bottom3");
+const arrowBottom4 = document.querySelector(".arrow-bottom4");
+const arrowBottom5 = document.querySelector(".arrow-bottom5");
+const slide = document.querySelector(".bio");
+const slide2 = document.querySelector(".bio2");
+const slide3 = document.querySelector(".bio3");
+const slide4 = document.querySelector(".bio4");
+const slide5 = document.querySelector(".bio5");
+
+function renameClassAll() {
+    slide.classList.toggle("bio-on");
+}
+
+function renameArrowAll() {
+    arrowBottom.classList.toggle('arrow')
+}
+
+arrowBottom.addEventListener("click", renameArrowAll)
+arrowBottom.addEventListener("click", renameClassAll)
+// ============================================
+
+function renameClassAll2() {
+    slide2.classList.toggle("bio-on");
+}
+
+function renameArrowAll2() {
+    arrowBottom2.classList.toggle('arrow')
+}
+
+arrowBottom2.addEventListener("click", renameArrowAll2)
+arrowBottom2.addEventListener("click", renameClassAll2)
+// ==============================================
+
+function renameClassAll3() {
+    slide3.classList.toggle("bio-on");
+}
+
+function renameArrowAll3() {
+    arrowBottom3.classList.toggle('arrow')
+}
+
+arrowBottom3.addEventListener("click", renameArrowAll3)
+arrowBottom3.addEventListener("click", renameClassAll3)
+// ==============================================
+
+function renameClassAll4() {
+    slide4.classList.toggle("bio-on");
+}
+
+function renameArrowAll4() {
+    arrowBottom4.classList.toggle('arrow')
+}
+
+arrowBottom4.addEventListener("click", renameArrowAll4)
+arrowBottom4.addEventListener("click", renameClassAll4)
+// ===============================================
+
+function renameClassAll5() {
+    slide5.classList.toggle("bio-on");
+}
+
+function renameArrowAll5() {
+    arrowBottom5.classList.toggle('arrow')
+}
+
+arrowBottom5.addEventListener("click", renameArrowAll5)
+arrowBottom5.addEventListener("click", renameClassAll5)
+
+
+
+
+
+ 
